65663faf35914b25fb96ec200114be887c29106e
Tom Janssens <tom@timble.net>
2015-01-24 16:29:45 +0100

    Initial commit

---
+++ code/Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+    // load time-grunt and all grunt plugins found in the package.json
+    require( 'load-grunt-tasks' )( grunt );
+
+    grunt.initConfig({
+        // Sass
+        sass: {
+            dist: {
+                files: {
+                    'css/default.css': 'css/default.scss'
+                }
+            }
+        },
+
+        // Watch
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass'],
+                options: {
+                    interrupt: false,
+                    atBegin: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+};
